e3b50e54fa5ce6fd07a09bca84c616be3a5d3951
playrey2010 <playrey2010@gmail.com>
2020-06-22 14:00:54 -0400

    retrieving user info and displaying on a page, tested

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/")
     public String index(){
@@ -26,6 +33,13 @@ public class HomeController {
     @RequestMapping("/admin")
     public String admin () {
         return "admin";
+    }
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
     }
 
 }
